9bf0fbf3d459effcb2baf718010c4c4eded30596
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-01-22 14:56:00 -0600

    commented out route until implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Homepage from "./components/homepage/Homepage";
-import Festival from "./components/festival/Festival"
+// import Festival from "./components/festival/Festival"
 import { Routes, Route} from 'react-router-dom';
 
 const App = () => {
@@ -9,7 +9,7 @@ const App = () => {
     <div style={{ maxWidth: "800px", margin: "0 auto", padding: "20px" }}>
       <Routes>
         <Route path="/" element={<Homepage/>} />
-        <Route path="/festivals/:festivalId" element={<Festival/>} />
+        {/* <Route path="/festivals/:festivalId" element={<Festival/>} /> */}
       </Routes>
     </div>
   );
