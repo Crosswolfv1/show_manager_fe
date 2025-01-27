8999e87df9169bee8448aae4ddb25449860a4ad6
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-01-27 17:14:55 -0600

    Added date abd time formatting

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -7,18 +7,36 @@ const Homepage = () => {
   const [filteredFestivalArray, setFilteredFestivalArray] = useState([]);
   const [nameFilter, setNameFilter] = useState("");
 
-  // Fetch festivals from the API
   useEffect(() => {
     fetch("http://localhost:3000/api/v1/festivals")
       .then((response) => response.json())
       .then((festivalData) => {
         const festivals = festivalData.data; 
-        setFestivalArray(festivals); 
-        setFilteredFestivalArray(festivals); 
+
+        const formattedFestivals = festivals.map(festival => {
+          const startDate = new Date(festival.attributes.start_time);
+          const endDate = new Date(festival.attributes.end_time);
+
+          festival.attributes.start_time = startDate.toLocaleDateString('en-US', {
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric',
+          });
+
+          festival.attributes.end_time = endDate.toLocaleDateString('en-US', {
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric',
+          });
+
+          return festival;
+        });
+
+        setFestivalArray(formattedFestivals);
+        setFilteredFestivalArray(formattedFestivals);
       })
       .catch((error) => console.error("Error fetching festivals:", error));
   }, []);
-
 
   useEffect(() => {
       const results = festivalArray.filter((festival) =>
